74008f3e8996734147fb2e566078b12ea894da6b
penguinwan <penguin_wan@yahoo.com.sg>
2021-01-18 22:10:36 +0100

    login

--- src/env-template.js
+++ src/env-template.js
@@ -1,2 +1,4 @@
 exports.PATH = 'https://{id}.execute-api.eu-central-1.amazonaws.com/Prod';
 //exports.PATH = 'http://localhost:8080';
+exports.USERNAME = '{username}';
+exports.PASSWORD = '{password}'
